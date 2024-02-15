cfecfb5e77d4aa148817c59e1943e273e362f9d9
Juan Jumbo <tiniyan24@gmail.com>
2024-02-15 01:35:44 -0500

    Tarea12

--- src/helpers/clienteEstudiante.js
+++ src/helpers/clienteEstudiante.js
@@ -1,24 +1,46 @@
 import axios from "axios";
-
-
-const consultarEstudiante= async(id)=>{
-    //axios tiene incluido implicitamente un await
-    //No hace falta declararlo
-    const data = axios.get(`http://localhost:8080/API/v1.0/Matricula/estudiantes/completo/${id}`).then(r => r.data);
-    console.log(data);
-    return data;
-}
-
-const insertar = async(body) => {
-    const data=axios.post(`http://localhost:8080/API/v1.0/Matricula/estudiantes`,body).then(r => r.data)
-    console.log(data)
-}
-
-export const consultarEstudianteFachada= async(id)=>{
-    return await consultarEstudiante(id);
-}
-
-export const insertarFachada = async(body) =>{
-    await insertar (body);
-}
-
+const consultarPorId = async (id) => {
+    const datos = axios
+      .get(`http://localhost:8080/API/v1.0/Matricula/estudiantes/completo/${id}`)
+      .then((r) => r.data);
+    return datos;
+  };
+  
+  const insertar = async (body) => {
+    axios
+      .post(`http://localhost:8080/API/v1.0/Matricula/estudiantes`, body)
+      .then((r) => r.data);
+  };
+  // const actualizar = async (id) => {
+  //   axios
+  //     .put(`http://localhost:8080/API/v1.0/Matricula/estudiantes/${id}`)
+  //     .then((r) => r.data);
+  // };
+  // const eliminar = async (id) => {
+  //   axios
+  //     .delete(`http://localhost:8080/API/v1.0/Matricula/estudiantes/${id}`)
+  //     .then((r) => r.data);
+  // };
+  
+  // const consultarTodos = async () => {
+  //     return axios
+  //       .get(`http://localhost:8080/API/v1.0/Matricula/estudiantes`)
+  //       .then((r) => r.data);
+  //   };
+  
+  export const consultarPorIdFachada = async (id) => {
+    return await consultarPorId(id);
+  };
+  
+  export const insertarFachada = async (body) => {
+    return await insertar(body);
+  };
+  // export const actualizarFachada = async (id) => {
+  //   return await consultarPorId(id);
+  // };
+  // export const eliminarFachada = async (id) => {
+  //   return await consultarPorId(id);
+  // };
+  // export const consultarTodosFachada = async () => {
+  //     return await consultarTodos();
+  //   };
